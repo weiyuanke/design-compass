20dfe94f138d4fb86bae16887de043f08b68fb61
weiyuanke <weiyuanke123@gmail.com>
2026-04-03 09:46:50 +0800

    fix

---
+++ src/crypto-polyfill.ts
@@ -0,0 +1,16 @@
+// Polyfill for crypto.randomUUID in non-secure contexts (HTTP)
+// Firefox and some other browsers don't provide crypto.randomUUID in non-secure contexts
+if (typeof crypto !== "undefined" && !crypto.randomUUID) {
+  crypto.randomUUID = function randomUUID(): string {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) => {
+      const num = parseInt(c, 10);
+      const randomBits = (crypto.getRandomValues
+        ? crypto.getRandomValues(new Uint8Array(1))[0]
+        : Math.floor(Math.random() * 256)) &
+        (0xf >> (num / 4));
+      return (num ^ randomBits).toString(16);
+    });
+  };
+}
+
+export {};
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import "./crypto-polyfill";
 import App from "./App.tsx";
 import "./index.css";
 
